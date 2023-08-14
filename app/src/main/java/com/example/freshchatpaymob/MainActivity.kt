ed39b1d00194ab0ee77d712bd240192963d35710
El-Mancy <abdelrahmanelmancy@paymob.com>
2023-08-15 00:33:03 +0300

    Initial

---
+++ app/src/main/java/com/example/freshchatpaymob/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.freshchatpaymob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.freshchat.consumer.sdk.Freshchat
+import com.freshchat.consumer.sdk.FreshchatConfig
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val freshchatConfig = FreshchatConfig("e85b0e65-50c0-416f-a74a-f572755ca002", "5f478d19-a586-4969-960f-5350193ac7ae")
+        freshchatConfig.domain = "msdk.freshchat.com"
+        freshchatConfig.isCameraCaptureEnabled = true
+        freshchatConfig.isGallerySelectionEnabled = true
+        freshchatConfig.isResponseExpectationEnabled = true
+        freshchatConfig.isTeamMemberInfoVisible = true
+        freshchatConfig.isUserEventsTrackingEnabled = true
+        Freshchat.getInstance(applicationContext)?.init(freshchatConfig)
+        val chatButton = findViewById<Button>(R.id.chatButton)
+        chatButton.setOnClickListener {
+            // Open Freshchat chat UI here
+            Freshchat.showConversations(this)
+        }
+
+    }
+}
